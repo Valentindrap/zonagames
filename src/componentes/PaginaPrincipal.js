d917ca8f75a0b1b01ee657f069f471a720f53081
Valentindrap <valentindrap01@gmail.com>
2024-11-08 19:32:37 -0300

    first commit

--- src/componentes/PaginaPrincipal.js
+++ src/componentes/PaginaPrincipal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 import NavbarPage from "./NavbarPage";
 import "../componentes/PaginaPrincipal.css";
+import logo from "../img/Icon-black.png"
 
 const juegos = [
     {
@@ -9,19 +10,61 @@ const juegos = [
         nombre: "GTA 6",
         url: "https://www.onlinegames.io/games/2023/unity2/gta-simulator/index.html",
         calificacion: 4.5,
+        desarrollador: "Rockstar Games",
         descripcion: `El GTA 6 está aquí, papu.`,
         portada: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/03/gta-6-3282307.jpg?tf=3840x"
     },
-    { id: 2, nombre: "Get On Top", url: "https://www.onlinegames.io/games/2024/code/6/get-on-top/index.html", calificacion: 4.0, descripcion: "a ver quien es mas locon", portada: "https://img.poki-cdn.com/cdn-cgi/image/quality=78,width=1200,height=1200,fit=cover,f=png/3078616e30ecc5f326bd8e6cad728a03.png" },
-    { id: 3, nombre: "Head Soccer 2022", url: "https://www.onlinegames.io/games/2023/construct/280/head-soccer-2022/index.html", calificacion: 5.0, descripcion: "el 1 con la WASD Y LA N Y M. El otro con la flechita y el 1 y 2. a juga", portada: "https://static.playhop.com/images/cc987_2977039_227e0/84f3ca624/2a00000180d6ac39_3fe781f/1329a0cbcf3a908ddae0_4d36e2/orig" },
-    { id: 4, nombre: "Draw Here", url: "https://www.onlinegames.io/games/2021/unity2/draw-here/index.html", calificacion: 5.0, descripcion: "NOSE PERO TENES QUE DIBUJAR", portada: "https://www.onlinegames.io/media/posts/567/responsive/Draw-Here-lg.jpg" },
-    { id: 5, nombre: "G-SWITCH 3", url: "https://html5-games.io/game/g-switch-3/", calificacion: 5.0, descripcion: "El puto goty", portada: "https://play-lh.googleusercontent.com/ylSOEyk2tG390Pm4-IRUlMVtjNuYnxgX1aTm6ZxC_I6hCHjanvHFgZkCCw73sGE3lg" },
-    { id: 6, nombre: "Fuego y Agua", url: "https://pavel-skala.github.io/Fireboy-and-Watergirl/", calificacion: 5.0, descripcion: "Dos jovenes papuchos en escapa de la bestia", portada: "https://play-lh.googleusercontent.com/6N_ON50BZiEAe-ll2lM92NrVrgp5I5Ha6VI0a4Usw7uPmgEjL6tgJR6jWYUkkYgx2LM" },
+    {
+        id: 2,
+        nombre: "Get On Top",
+        url: "https://www.onlinegames.io/games/2024/code/6/get-on-top/index.html",
+        calificacion: 4.0,
+        desarrollador: "MangoJam Studios",
+        descripcion: "A ver quien es más locon.",
+        portada: "https://img.poki-cdn.com/cdn-cgi/image/quality=78,width=1200,height=1200,fit=cover,f=png/3078616e30ecc5f326bd8e6cad728a03.png"
+    },
+    {
+        id: 3,
+        nombre: "Head Soccer 2022",
+        url: "https://www.onlinegames.io/games/2023/construct/280/head-soccer-2022/index.html",
+        calificacion: 5.0,
+        desarrollador: "D&D Dream Studios",
+        descripcion: "El 1 con la WASD Y LA N Y M. El otro con la flechita y el 1 y 2. A jugar.",
+        portada: "https://static.playhop.com/images/cc987_2977039_227e0/84f3ca624/2a00000180d6ac39_3fe781f/1329a0cbcf3a908ddae0_4d36e2/orig"
+    },
+    {
+        id: 4,
+        nombre: "Draw Here",
+        url: "https://www.onlinegames.io/games/2021/unity2/draw-here/index.html",
+        calificacion: 5.0,
+        desarrollador: "Hyperbolic Studios",
+        descripcion: "NOSE PERO TENES QUE DIBUJAR.",
+        portada: "https://www.onlinegames.io/media/posts/567/responsive/Draw-Here-lg.jpg"
+    },
+    {
+        id: 5,
+        nombre: "G-SWITCH 3",
+        url: "https://html5-games.io/game/g-switch-3/",
+        calificacion: 5.0,
+        desarrollador: "Serius Games",
+        descripcion: "El puto GOTY.",
+        portada: "https://play-lh.googleusercontent.com/ylSOEyk2tG390Pm4-IRUlMVtjNuYnxgX1aTm6ZxC_I6hCHjanvHFgZkCCw73sGE3lg"
+    },
+    {
+        id: 6,
+        nombre: "Fuego y Agua",
+        url: "https://pavel-skala.github.io/Fireboy-and-Watergirl/",
+        calificacion: 5.0,
+        desarrollador: "Pavel Skala",
+        descripcion: "Dos jóvenes papuchos en escape de la bestia.",
+        portada: "https://play-lh.googleusercontent.com/6N_ON50BZiEAe-ll2lM92NrVrgp5I5Ha6VI0a4Usw7uPmgEjL6tgJR6jWYUkkYgx2LM"
+    },
     {
         id: 7,
         nombre: "Clicker Heroes",
         url: "https://clickerheroes.com/",
         calificacion: 4.7,
+        desarrollador: "RPG Studios",
         descripcion: "Haz clic para derrotar a monstruos y desbloquear héroes en tu camino hacia la victoria.",
         portada: "https://play-lh.googleusercontent.com/XqysWlDWj5nBr0zZMtB08Bt4pe3MsE8XXOAgqZYiD1s65phAUWUcLIBLxdiAkfjfIsM"
     },
@@ -30,6 +73,7 @@ const juegos = [
         nombre: "Mr. Mine",
         url: "https://mrmine.com/game/",
         calificacion: 4.5,
+        desarrollador: "MineQuest Studio",
         descripcion: "Excava y descubre tesoros ocultos mientras gestionas tus recursos en esta divertida aventura minera.",
         portada: "https://f4.bcbits.com/img/a0036462721_10.jpg"
     },
@@ -38,6 +82,7 @@ const juegos = [
         nombre: "Kong Poker Quest",
         url: "https://playsaurus.com/kongPokerQuest63/",
         calificacion: 4.6,
+        desarrollador: "Plausible Games",
         descripcion: "Pon a prueba tus habilidades de póker en este emocionante juego lleno de estrategia y acción.",
         portada: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUS2BfMAGf_2hMfD1hl4D4gZfVL8kNtfQ0Uw&s"
     },
@@ -46,6 +91,7 @@ const juegos = [
         nombre: "Grindcraft",
         url: "https://grindcraft.com/game.php",
         calificacion: 4.8,
+        desarrollador: "CraftWorks Studio",
         descripcion: "Recoge recursos, crea herramientas y construye estructuras en este adictivo juego de crafting.",
         portada: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCGO_dQvYqOOnJ35mO4pXthfrOGgIo1ad-p0p7Zg-i_XhrsUmukI52LbOatTbrW4TP0kU&usqp=CAU"
     },
@@ -54,6 +100,7 @@ const juegos = [
         nombre: "Fray Fight",
         url: "https://frayfight.com/game/",
         calificacion: 4.4,
+        desarrollador: "Frenzy Fight Studios",
         descripcion: "Lucha con personajes únicos en este emocionante juego de peleas multijugador, donde solo los mejores sobrevivirán.",
         portada: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDpLK1V9DYinRPqt4P36ZpfhUWsPlqIHiBHA&s"
     },
@@ -62,6 +109,7 @@ const juegos = [
         nombre: "Zuno",
         url: "https://zv1y2i8p.play.gamezop.com/g/ByQxJnp7qRB",
         calificacion: 4.3,
+        desarrollador: "Gamezop Studios",
         descripcion: "Adéntrate en un mundo lleno de desafíos y diversión en esta emocionante aventura.",
         portada: "https://static.gamezop.com/ByQxJnp7qRB/cover.jpg"
     },
@@ -70,6 +118,7 @@ const juegos = [
         nombre: "Merge Mania",
         url: "https://zv1y2i8p.play.gamezop.com/g/hfPOimYqY",
         calificacion: 4.3,
+        desarrollador: "Fusion Labs",
         descripcion: "Combina elementos para avanzar en una serie de emocionantes retos y sorpresas.",
         portada: "https://static.gamezop.com/hfPOimYqY/cover.jpg"
     },
@@ -78,6 +127,7 @@ const juegos = [
         nombre: "Blazin Blades",
         url: "https://zv1y2i8p.play.gamezop.com/g/UYiznUAya",
         calificacion: 4.3,
+        desarrollador: "Epic Action Games",
         descripcion: "Embárcate en una aventura épica llena de acción y batallas emocionantes.",
         portada: "https://static.gamezop.com/UYiznUAya/cover.jpg"
     },
@@ -86,6 +136,7 @@ const juegos = [
         nombre: "Candy Fiesta",
         url: "https://zv1y2i8p.play.gamezop.com/g/r1zG1h6m90H",
         calificacion: 4.3,
+        desarrollador: "Sugar Rush Studios",
         descripcion: "Únete a una colorida fiesta de dulces llena de aventuras y desafíos.",
         portada: "https://static.gamezop.com/r1zG1h6m90H/cover.jpg"
     },
@@ -94,6 +145,7 @@ const juegos = [
         nombre: "Fruit Chop",
         url: "https://zv1y2i8p.play.gamezop.com/g/rkWfy2pXq0r",
         calificacion: 4.3,
+        desarrollador: "FruitSlice Inc.",
         descripcion: "Corta frutas en un juego de velocidad y precisión que pondrá a prueba tus reflejos.",
         portada: "https://static.gamezop.com/rkWfy2pXq0r/cover.jpg"
     },
@@ -102,6 +154,7 @@ const juegos = [
         nombre: "Zombies Can't Jump 2",
         url: "https://zv1y2i8p.play.gamezop.com/g/rkxMV8TI6Wg",
         calificacion: 4.3,
+        desarrollador: "Zombie Studios",
         descripcion: "Supera obstáculos y resuelve acertijos en un mundo donde los zombis no pueden saltar.",
         portada: "https://static.gamezop.com/rkxMV8TI6Wg/cover.jpg"
     },
@@ -110,6 +163,7 @@ const juegos = [
         nombre: "Bouncy",
         url: "https://zv1y2i8p.play.gamezop.com/g/H1Tz6z1Dqym",
         calificacion: 4.3,
+        desarrollador: "Bouncy Games",
         descripcion: "Salta y rebota en un divertido juego lleno de desafíos dinámicos.",
         portada: "https://static.gamezop.com/rkxMV8TI6Wg/cover.jpg"
     },
@@ -118,6 +172,7 @@ const juegos = [
         nombre: "Solitaire Gold",
         url: "https://zv1y2i8p.play.gamezop.com/g/rkPlk2T7qAr",
         calificacion: 4.3,
+        desarrollador: "Solitaire Studios",
         descripcion: "Disfruta de una experiencia clásica de solitario con un toque de oro y recompensas.",
         portada: "https://static.gamezop.com/rkPlk2T7qAr/cover.jpg"
     },
@@ -126,6 +181,7 @@ const juegos = [
         nombre: "Dunk Draw",
         url: "https://zv1y2i8p.play.gamezop.com/g/r1xZyhTQ50r",
         calificacion: 4.3,
+        desarrollador: "Basket Studios",
         descripcion: "Combina habilidades de dibujo y baloncesto en este divertido desafío.",
         portada: "https://static.gamezop.com/r1xZyhTQ50r/cover.jpg"
     },
@@ -134,6 +190,7 @@ const juegos = [
         nombre: "Pool Master",
         url: "https://zv1y2i8p.play.gamezop.com/g/hgempP8Sc",
         calificacion: 4.3,
+        desarrollador: "Cue Masters",
         descripcion: "Conviértete en un maestro del billar y supera a tus oponentes en emocionantes partidas.",
         portada: "https://static.gamezop.com/hgempP8Sc/cover.jpg"
     },
@@ -142,6 +199,7 @@ const juegos = [
         nombre: "Ludo with Friends",
         url: "https://zv1y2i8p.play.gamezop.com/g/SkhljT2fdgb",
         calificacion: 4.3,
+        desarrollador: "Ludo Labs",
         descripcion: "Disfruta del clásico juego de Ludo con amigos en una experiencia interactiva.",
         portada: "https://static.gamezop.com/SkhljT2fdgb/cover.jpg"
     },
@@ -150,6 +208,7 @@ const juegos = [
         nombre: "Slide and Divide",
         url: "https://zv1y2i8p.play.gamezop.com/g/PLQTtp9Ei",
         calificacion: 4.3,
+        desarrollador: "Puzzle Masters",
         descripcion: "Disfruta de una experiencia de puzzle única que desafiará tu mente.",
         portada: "https://static.gamezop.com/PLQTtp9Ei/cover.jpg"
     },
@@ -181,7 +240,6 @@ function PaginaPrincipal() {
         <div className="pagina-principal">
             <NavbarPage />
             <div className="contenedor-anuncios">
-                <div className="anuncio"></div>
                 <div className="catalogo-juegos">
                     <h1 className="juegos-titulo">Catálogo de Juegos</h1>
                     <div className="search-container">
@@ -211,8 +269,24 @@ function PaginaPrincipal() {
                         )}
                     </ul>
                 </div>
-                <div className="anuncio"></div>
             </div>
+            {/* Footer */}
+            <footer className="footer">
+            <img src={logo} className="logo"></img>
+            <p className="derechos">&copy; 2024 Todos los derechos reservados.</p>
+                <div className="footer-content">
+                    <p className="info">¿Quiénes somos?</p>
+                    <h3 className="info-abajo">Somos Valentin Drapanti y Francisco Pansa, dos estudiantes de la E.P.E.T N°3 con una gran pasión por los videojuegos y la tecnología. Juntos creamos Zona Games, una plataforma en línea que reúne una selección de juegos para todos los gustos.
+
+Nuestro objetivo es ofrecer una experiencia divertida, fácil de usar y accesible para todos los jugadores. En Zona Games trabajamos constantemente para mejorar la página y seguir aprendiendo, para que todos puedan disfrutar del mejor contenido y conectarse con la comunidad gamer.</h3>
+                    <div className="footer-links">
+                        <a href="/privacidad">Política de Privacidad</a>
+                        <a href="/terminos">Términos y Condiciones</a>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
--- src/componentes/PestañaJuegos.js
+++ src/componentes/PestañaJuegos.js
@@ -1,13 +1,92 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";  // Importa useNavigate
 import NavbarPage from "./NavbarPage";
 import "../componentes/PestañaJuegos.css";
+import logo from "../img/Icon-black.png";
+
+const juegos = [
+    {
+        id: 1,
+        nombre: "GTA 6",
+        url: "https://www.onlinegames.io/games/2023/unity2/gta-simulator/index.html",
+        calificacion: 4.5,
+        desarrollador: "Rockstar Games",
+        descripcion: `El GTA 6 está aquí, papu.`,
+        portada: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/03/gta-6-3282307.jpg?tf=3840x"
+    },
+    {
+        id: 2,
+        nombre: "Get On Top",
+        url: "https://www.onlinegames.io/games/2024/code/6/get-on-top/index.html",
+        calificacion: 4.0,
+        desarrollador: "MangoJam Studios",
+        descripcion: "A ver quien es más locon.",
+        portada: "https://img.poki-cdn.com/cdn-cgi/image/quality=78,width=1200,height=1200,fit=cover,f=png/3078616e30ecc5f326bd8e6cad728a03.png"
+    },
+    {
+        id: 3,
+        nombre: "Head Soccer 2022",
+        url: "https://www.onlinegames.io/games/2023/construct/280/head-soccer-2022/index.html",
+        calificacion: 5.0,
+        desarrollador: "D&D Dream Studios",
+        descripcion: "El 1 con la WASD Y LA N Y M. El otro con la flechita y el 1 y 2. A jugar.",
+        portada: "https://static.playhop.com/images/cc987_2977039_227e0/84f3ca624/2a00000180d6ac39_3fe781f/1329a0cbcf3a908ddae0_4d36e2/orig"
+    },
+    {
+        id: 4,
+        nombre: "Draw Here",
+        url: "https://www.onlinegames.io/games/2021/unity2/draw-here/index.html",
+        calificacion: 5.0,
+        desarrollador: "Hyperbolic Studios",
+        descripcion: "NOSE PERO TENES QUE DIBUJAR.",
+        portada: "https://www.onlinegames.io/media/posts/567/responsive/Draw-Here-lg.jpg"
+    },
+    {
+        id: 5,
+        nombre: "G-SWITCH 3",
+        url: "https://html5-games.io/game/g-switch-3/",
+        calificacion: 5.0,
+        desarrollador: "Serius Games",
+        descripcion: "El puto GOTY.",
+        portada: "https://play-lh.googleusercontent.com/ylSOEyk2tG390Pm4-IRUlMVtjNuYnxgX1aTm6ZxC_I6hCHjanvHFgZkCCw73sGE3lg"
+    },
+    {
+        id: 6,
+        nombre: "Fuego y Agua",
+        url: "https://pavel-skala.github.io/Fireboy-and-Watergirl/",
+        calificacion: 5.0,
+        desarrollador: "Pavel Skala",
+        descripcion: "Dos jóvenes papuchos en escape de la bestia.",
+        portada: "https://play-lh.googleusercontent.com/6N_ON50BZiEAe-ll2lM92NrVrgp5I5Ha6VI0a4Usw7uPmgEjL6tgJR6jWYUkkYgx2LM"
+    },
+    {
+        id: 7,
+        nombre: "Clicker Heroes",
+        url: "https://clickerheroes.com/",
+        calificacion: 4.7,
+        desarrollador: "RPG Studios",
+        descripcion: "Haz clic para derrotar a monstruos y desbloquear héroes en tu camino hacia la victoria.",
+        portada: "https://play-lh.googleusercontent.com/XqysWlDWj5nBr0zZMtB08Bt4pe3MsE8XXOAgqZYiD1s65phAUWUcLIBLxdiAkfjfIsM"
+    },
+    {
+        id: 8,
+        nombre: "Mr. Mine",
+        url: "https://mrmine.com/game/",
+        calificacion: 4.5,
+        desarrollador: "MineQuest Studio",
+        descripcion: "Excava y descubre tesoros ocultos mientras gestionas tus recursos en esta divertida aventura minera.",
+        portada: "https://f4.bcbits.com/img/a0036462721_10.jpg"
+    },
+    // ... Aquí incluirías todos los demás juegos del array `juegos` que ya tienes
+];
 
 function PestañaJuegos() {
     const location = useLocation();
     const juegoSeleccionado = location.state?.juego;
     const gameContainerRef = useRef(null);
     const [isFavorito, setIsFavorito] = useState(false);
+    const [isFullScreen, setIsFullScreen] = useState(false); // Estado para controlar el modo pantalla completa
+    const navigate = useNavigate();  // Crea la función para navegar
 
     const toggleFullScreen = () => {
         if (!document.fullscreenElement) {
@@ -41,6 +120,25 @@ function PestañaJuegos() {
         setIsFavorito(isFav);
     }, [juegoSeleccionado]);
 
+    useEffect(() => {
+        // Función para manejar el cambio de estado de pantalla completa
+        const handleFullScreenChange = () => {
+            if (document.fullscreenElement) {
+                setIsFullScreen(true); // Se entra en pantalla completa
+            } else {
+                setIsFullScreen(false); // Se sale de pantalla completa
+            }
+        };
+
+        // Escuchar el evento de cambio de pantalla completa
+        document.addEventListener('fullscreenchange', handleFullScreenChange);
+
+        // Limpiar el evento cuando el componente se desmonta
+        return () => {
+            document.removeEventListener('fullscreenchange', handleFullScreenChange);
+        };
+    }, []);
+
     if (!juegoSeleccionado) {
         return <div>No se seleccionó ningún juego.</div>;
     }
@@ -50,37 +148,77 @@ function PestañaJuegos() {
             <NavbarPage />
             <div className="contenedor-anuncios">
                 <div className="anuncio"></div>
-                <div className="PestañaJuegos-container">
-                    <div className="MedPestañaJuegos-container" ref={gameContainerRef}>
-                        <div className="fullscreen-btn-container">
-                            <button className="btn btn-primary fullscreen-btn" onClick={toggleFullScreen}>
-                                <i className="bi bi-fullscreen "></i>
-                                {document.fullscreenElement ? " Salir de Pantalla Completa" : " Pantalla Completa"}
+                  {/* Botón para retroceder a la página principal */}
+                  <div className="back-btn-container">
+                            <button className="btn btn-secondary" onClick={() => navigate('/')}>
+                                <i className="bi bi-arrow-left-circle"></i>
                             </button>
                         </div>
+                <div className="PestañaJuegos-container">
+                    <div className="MedPestañaJuegos-container" ref={gameContainerRef}>
+
+                        {/* Solo mostrar el botón de pantalla completa si no estamos en pantalla completa */}
+                        {!isFullScreen && (
+                            <div className="fullscreen-btn-container">
+                                <button className="btn btn-primary fullscreen-btn" onClick={toggleFullScreen}>
+                                    <i className="bi bi-fullscreen "></i>
+                                    {document.fullscreenElement ? " Salir de Pantalla Completa" : ""}
+                                </button>
+                            </div>
+                        )}
+                        
                         <iframe
                             src={juegoSeleccionado.url}
                             title={juegoSeleccionado.nombre}
-                        ></iframe >
+                        ></iframe>
                         <div className="Nombre-PestañaJuegos">
                             <div className="IzqNombre-PestañaJuegos">
                                 <p>{juegoSeleccionado.nombre}</p>
                                 <button onClick={handleFavoritoClick} className="btn btn-favoritos">
-                                   {isFavorito ? "Eliminar de Favoritos" : "Agregar a Favoritos"}
+                                    {isFavorito ? "Eliminar de Favoritos" : "Agregar a Favoritos"}
                                 </button>
                             </div>
                             <div className="DerNombre-PestañaJuegos">
-                                <p><i className="bi bi-star"></i> Calificación: {juegoSeleccionado.calificacion}</p>
+                                <p>{juegoSeleccionado.desarrollador}</p>
+                                <p className="calificacion"><i className="bi bi-star"></i> Calificación: {juegoSeleccionado.calificacion}</p>
                             </div>
-                        </div>
-                        <div className="Abajo-PestañaJuegos">
-                            <h3>Descripción</h3>
-                            <p>{juegoSeleccionado.descripcion}</p>
+                            <h3 className="descripcion">Descripción</h3>
+                            <p className="descripcion-abajo">{juegoSeleccionado.descripcion}</p>
                         </div>
                     </div>
                 </div>
-                <div className="anuncio"></div>
+
+                {/* Agregamos el catálogo de juegos justo antes del footer */}
+                <div className="catalogo-juegos">
+                    <h2 className="juegos-titulo">Juegos Recomendados</h2>
+                    <ul className="lista-juegos">
+                        {juegos.map((juego) => (
+                            <li key={juego.id} onClick={() => navigate(`/juego/${juego.id}`, { state: { juego } })} className="catalogo-juegos-item">
+                                <img src={juego.portada} alt={`Portada de ${juego.nombre}`} />
+                                <h3>{juego.nombre}</h3>
+                                <p>Calificación: {juego.calificacion}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
+            {/* Footer */}
+            <footer className="footer">
+                <img src={logo} className="logo" alt="Logo" />
+                <p className="derechos">&copy; 2024 Todos los derechos reservados.</p>
+                <div className="footer-content">
+                    <p className="info">¿Quiénes somos?</p>
+                    <h3 className="info-abajo">Somos Valentin Drapanti y Francisco Pansa, dos estudiantes de la E.P.E.T N°3 con una gran pasión por los videojuegos y la tecnología. Juntos creamos Zona Games, una plataforma en línea que reúne una selección de juegos para todos los gustos.
+
+Nuestro objetivo es ofrecer una experiencia divertida, fácil de usar y accesible para todos los jugadores. En Zona Games trabajamos constantemente para mejorar la página y seguir aprendiendo, para que todos puedan disfrutar del mejor contenido y conectarse con la comunidad gamer.</h3>
+                    <div className="footer-links">
+                        <a href="/privacidad">Política de Privacidad</a>
+                        <a href="/terminos">Términos y Condiciones</a>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
